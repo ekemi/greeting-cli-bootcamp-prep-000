c1a3a8a4add4c851c48293e20a02c9a2934fee07
edmundo papi <edmundopierre@yahoo.com>
2020-03-06 02:49:46 +0000

    Automatically backed up by Learn

--- fibo.rb
+++ fibo.rb
@@ -4,8 +4,14 @@
 #  puts "*  "*(i+1)
 # # puts "* "*(i+1)
 #  }
+7.times{|i|
+  
+  i = Random.new.rand(3..6)
+  puts i
 
-puts Random.new.rand(1..5)
+  
+}
+#puts Random.new.rand(1..5)
 #(5).times{|i| puts"*" * count
 #count-=1}
 
